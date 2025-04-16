94f56c91b4d008a3a8b63aa6b80a69176da1ed39
chetansohlot <chetan.k@netleafinfosoft.com>
2025-04-16 10:26:36 +0530

    uplad

--- empPortal/models.py
+++ empPortal/models.py
@@ -277,7 +277,7 @@ class PolicyInfo(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     class Meta:
         db_table = "policy_info"
 
