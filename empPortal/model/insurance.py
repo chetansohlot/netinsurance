be71daa952d84a7124fd30e2b9f4d53f9210a402
anjalipandey14 <anjali.pandey@netleafsoftware.com>
2025-05-15 19:12:32 +0530

    Anjali

--- empPortal/model/insurance.py
+++ empPortal/model/insurance.py
@@ -15,8 +15,28 @@ class Insurance(models.Model):
     pincode = models.IntegerField(null=True, blank=True, db_column='insurance_type_pincode')
     address = models.TextField(null=True, blank=True, db_column='insurance_type_address')
     commencement_date = models.DateField(null=True, blank=True, db_column='insurance_type_commencement_date')
-    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True, db_column='insurance_type_state')
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True, db_column='insurance_type_city')
+    #state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True, db_column='insurance_type_state')
+    #city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True, db_column='insurance_type_city')
+    state = models.CharField(max_length=100, null=True, blank=True, db_column='insurance_type_state')
+    city = models.CharField(max_length=100, null=True, blank=True, db_column='insurance_type_city')
+    billing_state = models.CharField(max_length=100, null=True, blank=True)
+    billing_city = models.CharField(max_length=100, null=True, blank=True)
+    #billing_state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True, related_name='billing_states')
+    #billing_city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True, related_name='billing_cities')
+    billing_pincode = models.IntegerField(null=True, blank=True)
+    billing_address = models.TextField(null=True, blank=True)
+    billing_same_as_registered = models.BooleanField(default=False)  
+     # Primary Contact Person Details
+    primary_contact_name = models.CharField(max_length=255, null=True, blank=True)
+    primary_designation = models.CharField(max_length=255, null=True, blank=True)
+    primary_contact_no = models.BigIntegerField(null=True, blank=True)
+    primary_contact_email = models.CharField(max_length=255, null=True, blank=True)
+
+    # Secondary Contact Person Details
+    secondary_contact_name = models.CharField(max_length=255, null=True, blank=True)
+    secondary_designation = models.CharField(max_length=255, null=True, blank=True)
+    secondary_contact_no = models.BigIntegerField(null=True, blank=True)
+    secondary_contact_email = models.CharField(max_length=255, null=True, blank=True)
 
 
     def __str__(self):
